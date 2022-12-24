d7634b2f1e481d65879e2adb8ffe35a33fcf608f
Masoud <hosseiny998@gmail.com>
2022-12-24 20:26:31 +0330

    add sevice station apis

---
+++ controllers/api/v1/serviceStationController.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"man-CRM/models"
+	"net/http"
+)
+
+func ServiceIndex(c *gin.Context) {
+
+	services := models.GetDB().Offset(3).Find(&models.Service{})
+	c.JSON(http.StatusOK, services)
+}
+
+func ServiceCreate(c *gin.Context) {
+	//status := models.GetDB().First(&models.Status{})
+
+	service := models.GetDB().Create(&models.Service{
+		Name:        c.PostForm("name"),
+		Description: c.PostForm("description"),
+	})
+	c.JSON(http.StatusOK, service.Value)
+
+}
+
+func ServiceShow(c *gin.Context) {
+	service := models.GetDB().First(&models.Service{}, c.Param("id"))
+	c.JSON(http.StatusOK, service)
+}
